9b94a78846a76df102955a3b228770a788b9ca80
Laurynas Baltrusaitis <laurynas.baltrusaitis@adptfy.com>
2026-01-29 11:19:24 +0530

    Formatted the py files

--- Ch2 Simple ML algo for Classification/helper_functions.py
+++ Ch2 Simple ML algo for Classification/helper_functions.py
@@ -2,17 +2,19 @@ from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_decision_regions(X, y, classifier, resolution = 0.02):
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
     # Set up marker generator and color map
-    markers = ('o', 's', '^', 'v', '<')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
+    markers = ("o", "s", "^", "v", "<")
+    colors = ("red", "blue", "lightgreen", "gray", "cyan")
+    cmap = ListedColormap(colors[: len(np.unique(y))])
 
     # plot the decision surface
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                           np.arange(x2_min, x2_max, resolution))
+    xx1, xx2 = np.meshgrid(
+        np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution)
+    )
 
     lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     lab = lab.reshape(xx1.shape)
@@ -21,12 +23,12 @@ def plot_decision_regions(X, y, classifier, resolution = 0.02):
     plt.ylim(xx2.min(), xx2.max())
     # plot class examples
     for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0],
-                    y=X[y == cl, 1],
-                    alpha=0.8,
-                    c=colors[idx],
-                    marker=markers[idx],
-                    label=f'Class {cl}',
-                    edgecolor='black')
-
-  
+        plt.scatter(
+            x=X[y == cl, 0],
+            y=X[y == cl, 1],
+            alpha=0.8,
+            c=colors[idx],
+            marker=markers[idx],
+            label=f"Class {cl}",
+            edgecolor="black",
+        )
