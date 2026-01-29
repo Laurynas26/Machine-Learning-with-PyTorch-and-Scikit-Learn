eb154144d4c6244704c33caf981adac595615a26
Laurynas Baltrusaitis <laurynas.baltrusaitis@adptfy.com>
2026-01-29 11:20:33 +0530

    Formatted the py files

--- Ch2 Simple ML algo for Classification/adaline.py
+++ Ch2 Simple ML algo for Classification/adaline.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class AdalineGD:
     """ "
     Single layer neural network (NN) called ADAptive LInear NEuron (Adaline).
@@ -46,29 +47,26 @@ class AdalineGD:
         """
         rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
-        self.b_ = np.float64(0.)
+        self.b_ = np.float64(0.0)
         self.losses_ = []
 
         for i in range(self.n_iter):
             net_input = self.net_input(X)
             output = self.activation(net_input)
-            errors = (y - output)
+            errors = y - output
             self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
             self.b_ += self.eta * 2.0 * errors.mean()
-            loss = (errors ** 2).mean()
+            loss = (errors**2).mean()
             self.losses_.append(loss)
         return self
-
-
 
     def net_input(self, X):
         """Calculate net input."""
         return np.dot(X, self.w_) + self.b_
 
     def activation(self, X):
-        """"Compute linear activation."""
+        """ "Compute linear activation."""
         return X
-
 
     def predict(self, X):
         return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
